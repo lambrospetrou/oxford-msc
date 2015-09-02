4f2a8a92bb885620b95a384421671a82fb735a5f
Lambros Petrou <lambrospetrou@gmail.com>
2015-09-02 21:49:05 +0100

    removed unused stuff

--- ftrees/plot/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# first, install the following tool:
-# sudo apt-get install librsvg2-bin
-
-gnuplot script_plot
-
-rsvg-convert -f pdf -o housing-proper-withcompiler.pdf housing-proper-withcompiler.svg
